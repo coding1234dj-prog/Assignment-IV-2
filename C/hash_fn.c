9b0e4a9ef08eca7ff7a5f4faa3be80fa6db06a3b
DD <coding1234dj@gmail.com>
2025-11-23 18:12:43 +0800

    right ver 3

--- C/hash_fn.c
+++ C/hash_fn.c
@@ -25,3 +25,54 @@ int myHashString(const char* str, int m) {
     // TODO: replace with your own design
     return (int)(hash % m); // basic division method
 }
+
+
+/**
+ * @brief 計算整數鍵值 hash index
+ *
+ * 處理負數鍵值問題，確保回傳值永遠落在 [0, table_size-1]
+ * 建議 table_size 使用質數以獲得最佳分佈均勻度
+ *
+ * @param key         hash inter
+ * @param table_size  hash size
+ * @return            範圍 [0, table_size-1]  hash index
+ */
+
+int my_hash_int(int key, int table_size)
+{
+    if (table_size <= 0) {
+        return 0;  /// 錯誤情況直接回傳 
+    }
+    /// 處理負數：(key % m + m) % m 
+    return (key % table_size + table_size) % table_size;
+}
+
+
+
+
+/**
+ * @brief 計算字串鍵值的hash index
+ *
+ * 採用業界標準的 31 基演算法（Java、C#、Python 內建皆用此法）
+ * 參考資料：
+ *   - https://blog.csdn.net/qq_43382960/article/details/124071223
+ *   - https://www.cnblogs.com/looyee/articles/11424208.html
+ *
+ * @param str         
+ * @param table_size  必須 > 0
+ * @return            範圍 [0, table_size-1] 的 hash index
+ */
+
+int my_hash_string(const char* str, int table_size)
+{
+    if (str == NULL || table_size <= 0) {
+        return 0;
+    }
+
+    unsigned int hash = 0;  // 用 unsigned 防止溢位變負數
+    while (*str) {
+        hash = hash * 31 + (unsigned char)(*str);
+        str++;
+    }
+    return hash % table_size;
+}
--- CXX/hash_fn.cpp
+++ CXX/hash_fn.cpp
@@ -24,3 +24,43 @@ int myHashString(const std::string& str, int m) {
     // TODO: replace with your own design
     return static_cast<int>(hash % m);  // basic division method
 }
+
+
+/**
+ * @brief 計算整數鍵值 hash index
+ *
+ * 處理負數鍵值問題，確保回傳值永遠落在 [0, table_size-1]
+ * 建議 table_size 使用質數以獲得最佳分佈均勻度
+ *
+ * @param key         hash inter
+ * @param table_size  hash size
+ * @return            範圍 [0, table_size-1]  hash index
+ */
+
+int myHashInt(int key, int tableSize) {
+    return key % tableSize;
+}
+
+
+
+
+/**
+ * @brief 計算字串鍵值的hash index
+ *
+ * 採用業界標準的 31 基演算法（Java、C#、Python 內建皆用此法）
+ * 參考資料：
+ *   - https://blog.csdn.net/qq_43382960/article/details/124071223
+ *   - https://www.cnblogs.com/looyee/articles/11424208.html
+ *
+ * @param key         
+ * @param table_size  必須 > 0
+ * @return            範圍 [0, table_size-1] 的 hash index
+ */
+
+int myHashString(const std::string& str, int tableSize) {
+    int hash = 0;
+    for (char c : str) {
+        hash = hash * 31 + c;  // 處理中文或負的 char
+    }
+    return hash % tableSize;
+}
